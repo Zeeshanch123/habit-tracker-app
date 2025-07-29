fd501320df9fd33186dd7eaa82b03e6cc6937868
Zeeshanch123 <zeeshanch160@gmail.com>
2025-07-29 17:46:10 +0500

    main-ts

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 5000;
   await app.listen(port);
-  console.log(`✅ Server is running on ${port}`);
+  console.log(`✅ Server is running on ${port}!`);
   console.log('DB_HOST:', process.env.DB_HOST);
 }
 bootstrap();
